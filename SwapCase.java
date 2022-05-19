e6725440539bee5c6d92df395cdc5933e02d7a5f
Metin AKSU <drmetin28@hotmail.com>
2022-05-19 19:00:30 +0300

    Update SwapCase.java

--- SwapCase.java
+++ SwapCase.java
@@ -1,4 +1,9 @@
-
+/*
+Swap Case                                                                           
+Using the JavaScript language, have the function SwapCase(str) take the str         
+parameter and swap the case of each character. For example: if str is "Hello World"
+the output should be hELLO wORLD. Let numbers and symbols stay the way they are. 
+*/
 public class SwapCase {
 
     public static String swapCase(String str) {
