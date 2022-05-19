13a451dfa6e80e8d9cfc6bb087500c2accfc985b
Metin AKSU <drmetin28@hotmail.com>
2022-05-19 18:57:20 +0300

    Update ArrayAddition.java

--- ArrayAddition.java
+++ ArrayAddition.java
@@ -1,7 +1,10 @@
 /*
 Array Addition
 
-Have the function ArrayAddition(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array (excluding the largest number) can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, and may contain negative numbers. 
+Have the function ArrayAddition(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array 
+(excluding the largest number) can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr 
+contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, 
+and may contain negative numbers. 
 */
 
 import java.util.*; 
