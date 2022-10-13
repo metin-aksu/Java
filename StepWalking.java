55ad6d4ba6760168cb2b1f6fd9a172af338e17f2
Metin AKSU <drmetin@msn.com>
2022-10-13 16:23:43 +0300

    Update StepWalking.java

--- StepWalking.java
+++ StepWalking.java
@@ -1,6 +1,10 @@
 /*
 Step Walking
-Have the function StepWalking(num) take the num parameter being passed which will be an integer between 1 and 1,000 that represents the number of stairs you will have to climb. You can climb the set of stairs by taking either 1 step or 2 steps, and you can combine these in any order. So for example, to climb 3 steps you can either do: (1 step, 1 step, 1 step) or (2, 1) or (1, 2). So for 3 steps we have 3 different ways to climb them, so your program should return 3. Your program should return the number of combinations of climbing num steps.
+Have the function StepWalking(num) take the num parameter being passed which will be an integer between 1 and 1,000 that represents the 
+number of stairs you will have to climb. You can climb the set of stairs by taking either 1 step or 2 steps, and you can combine these in any order. 
+So for example, to climb 3 steps you can either do: (1 step, 1 step, 1 step) or (2, 1) or (1, 2). So for 3 steps we have 3 different ways to climb them, 
+so your program should return 3. Your program should return the number of combinations of climbing num steps.
+
 Examples
 Input: 1
 Output: 1
